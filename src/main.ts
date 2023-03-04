a105b41f1b1d87637f179e8a1076ffd20727b976
Hidetake Iwata <int128@gmail.com>
2023-03-04 18:20:18 +0900

    Initial import (#3)
    * Initial import
    
    * Add test
    
    * Add doc

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { run } from './run'
 
 const main = async (): Promise<void> => {
   await run({
-    name: core.getInput('name', { required: true }),
+    functionName: core.getInput('function-name', { required: true }),
+    image: core.getInput('image', { required: true }),
+    aliasName: core.getInput('alias-name'),
+    aliasDescription: core.getInput('alias-description'),
   })
 }
 
